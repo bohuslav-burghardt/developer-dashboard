6f10c851468a5cefed3353a84779d82107b28047
Bohuslav Burghardt <bohuslav.burghardt@jumio.com>
2021-12-06 11:58:44 +0100

    Changes

---
+++ developer-dashboard-backend/src/main/java/com/hackathon/developerdashboard/core/domain/JiraConfig.java
@@ -0,0 +1,12 @@
+package com.hackathon.developerdashboard.core.domain;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(fluent = true)
+public class JiraConfig {
+
+    private String personalToken;
+
+}
